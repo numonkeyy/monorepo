545ce7a1d233c070ac0a8e84e15bf5e35d370f72
LorisSigrist <loris@sigrist.dev>
2024-03-15 13:40:28 +0100

    Add basic `paraglide-js build` command

---
+++ inlang/source-code/paraglide/paraglide-js/src/cli/commands/build.ts
@@ -0,0 +1,111 @@
+import { Command } from "commander"
+import { openRepository, findRepoRoot } from "@lix-js/client"
+import { loadProject, type InlangProject } from "@inlang/sdk"
+import fs from "node:fs/promises"
+import { execSync } from "node:child_process"
+import { resolve } from "node:path"
+
+export const buildCommand = new Command()
+	.name("build")
+	.summary("Wraps your framework's build command to orchestrate building all languages separately")
+	.requiredOption("--project <path>", 'The path to the inlang project. Example: "./project.inlang"')
+	.requiredOption(
+		"--routes-dir <path>",
+		'Path to the folder containing your routes. Example: "./src/app/"'
+	)
+	.action(async (options: { project: string; routesDir: string }, env) => {
+		const projectPath = resolve(process.cwd(), options.project)
+		const routesDirectory = resolve(process.cwd(), options.routesDir)
+		const tmpDir = resolve(process.cwd(), ".tmp")
+		const buildCommand = env.args.join(" ")
+
+		const repoRoot = await findRepoRoot({ nodeishFs: fs, path: projectPath })
+		const repo = await openRepository(repoRoot || process.cwd(), {
+			nodeishFs: fs,
+		})
+
+		const project = exitIfErrors(
+			await loadProject({
+				projectPath,
+				repo,
+				appId: "library.inlang.paraglideJs",
+			})
+		)
+
+		const projectSettings = project.settings()
+
+		// copy the routes directory to a temporary directory
+		await fs.cp(routesDirectory, tmpDir, { recursive: true })
+
+		// resolve the directory for each language
+		const languageDirectories: Record<string, string> = {}
+		for (const language of projectSettings.languageTags) {
+			languageDirectories[language] = resolve(tmpDir, language)
+		}
+
+		// create a language directory for each language
+		for (const [, languageDir] of Object.entries(languageDirectories)) {
+			await repo.nodeishFs.cp(tmpDir, languageDir, { recursive: true })
+		}
+
+		// Walk through the language directories and rewrite imports
+		for (const [language, languageDir] of Object.entries(languageDirectories)) {
+			await walk(languageDir, ({ path, content }) => {
+				const extensions = ["js", "ts", "jsx", "tsx", "vue", "astro", "svelte", "svx"]
+				if (!extensions.includes(path.split(".").pop() as string)) return content
+				return content.replace("paraglide/messages", `paraglide/messages/${language}`)
+			})
+		}
+
+		try {
+			// run the build command
+			execSync(buildCommand, { stdio: "inherit" })
+		} catch (err) {
+			console.error(err)
+		}
+
+		// move the routes directory back
+		await cleanup(tmpDir, routesDirectory)
+
+		async function cleanup(tempDir: string, routesDir: string) {
+			await fs.rm(routesDir, { recursive: true, force: true })
+			await fs.cp(tempDir, routesDir, { recursive: true })
+			await fs.rm(tempDir, { recursive: true, force: true })
+		}
+	})
+
+/**
+ * Utility function to exit when the project has errors.
+ */
+const exitIfErrors = (project: InlangProject) => {
+	if (project.errors().length > 0) {
+		console.warn(`The project has errors:`)
+		for (const error of project.errors()) {
+			console.error(error)
+		}
+		process.exit(1)
+	}
+	return project
+}
+
+/**
+ * Recursively walk through a directory & rewrite all files in-place
+ */
+async function walk(
+	dir: string,
+	rewrite: (args: { path: string; content: string; depth: number }) => string,
+	depth = 0
+) {
+	const entries = await fs.readdir(dir)
+	for (const entry of entries) {
+		const path = resolve(dir, entry)
+		const stat = await fs.stat(path)
+		if (stat.isDirectory()) {
+			await walk(path, rewrite, depth + 1)
+		} else {
+			const content = await fs.readFile(path, "utf8")
+			const rewritten = rewrite({ path, content, depth })
+			await fs.writeFile(path, rewritten, { encoding: "utf-8" })
+		}
+	}
+}
--- inlang/source-code/paraglide/paraglide-js/src/cli/main.ts
+++ inlang/source-code/paraglide/paraglide-js/src/cli/main.ts
@@ -2,10 +2,12 @@ import { Command } from "commander"
 import { compileCommand } from "./commands/compile.js"
 import { initCommand } from "./commands/init.js"
 import { version } from "./state.js"
+import { buildCommand } from "./commands/build.js"
 
 export const cli = new Command()
 	.name("paraglide-js")
 	.addCommand(compileCommand)
 	.addCommand(initCommand)
+	.addCommand(buildCommand)
 	.showHelpAfterError()
 	.version(version)
