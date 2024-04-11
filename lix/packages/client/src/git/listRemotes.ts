d88702e46400381d655dbf9bfda48a3377be5ffd
Jan <jan.johannes@opral.com>
2024-04-12 02:09:20 +0300

    lix refactor
    Summary:
    
    Test Plan:

---
+++ lix/packages/client/src/git/listRemotes.ts
@@ -0,0 +1,28 @@
+import type { RepoContext, RepoState } from "../openRepository.js"
+import isoGit from "../../vendored/isomorphic-git/index.js"
+
+/**
+ * Gets the git origin url of the current repository.
+ *
+ * @returns The git origin url or undefined if it could not be found.
+ */
+export async function listRemotes(ctx: RepoContext, state: RepoState) {
+	try {
+		// const withProxypedFS =
+		// withProxy({
+		// 	nodeishFs: rawFs,
+		// 	verbose: debug,
+		// 	description: "listRemotes",
+		// 	intercept: delayedAction,
+		// })
+
+		const remotes = await isoGit.listRemotes({
+			fs: state.nodeishFs,
+			dir: ctx.dir,
+		})
+
+		return remotes
+	} catch (_err) {
+		return undefined
+	}
+}
