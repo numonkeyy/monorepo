4bc3b070755ea1d1a0061d8bdf85220fee152bef
Jan <jan.johannes@opral.com>
2024-05-23 20:23:04 +0300

    fix fink login

--- lix/packages/server/src/auth/router.ts
+++ lix/packages/server/src/auth/router.ts
@@ -7,15 +7,12 @@ export const router: Router = Router()
 const PUBLIC_LIX_GITHUB_APP_NAME = getEnvVar("PUBLIC_LIX_GITHUB_APP_NAME")
 const installUrl = `https://github.com/apps/${PUBLIC_LIX_GITHUB_APP_NAME}/installations/new`
 
-const PUBLIC_SERVER_BASE_URL = getEnvVar("PUBLIC_SERVER_BASE_URL", {
-	descirption: "The base url of the server e.g. https://inlang.com - Must not end with a slash",
-})
-const callbackUrl = `${PUBLIC_SERVER_BASE_URL}/services/auth/auth-callback`
-
 const PUBLIC_LIX_GITHUB_APP_CLIENT_ID = getEnvVar("PUBLIC_LIX_GITHUB_APP_CLIENT_ID")
 const LIX_GITHUB_APP_CLIENT_SECRET = getEnvVar("LIX_GITHUB_APP_CLIENT_SECRET")
 const PUBLIC_ALLOWED_AUTH_URLS = getEnvVar("PUBLIC_ALLOWED_AUTH_URLS")
 const JWE_SECRET = getEnvVar("JWE_SECRET")
+
+const allowedAuthUrls = PUBLIC_ALLOWED_AUTH_URLS.split(",")
 
 /**
  * OAuth flow from GitHub
@@ -24,8 +21,14 @@ const JWE_SECRET = getEnvVar("JWE_SECRET")
  * and that the route is set in the GitHub app settings.
  */
 
-const allowedAuthUrls = PUBLIC_ALLOWED_AUTH_URLS.split(",")
 router.get("/github-auth-callback", async (request, response, next) => {
+	const callbackUrl = decodeURI((request.query["state"] as string) || "") as string
+
+	const callBackOrigin = new URL(callbackUrl).origin
+	if (!allowedAuthUrls.includes(callBackOrigin)) {
+		return next(new Error("Origin not allowed"))
+	}
+
 	try {
 		// TODO: org installation
 		// TODO: test harness
@@ -70,7 +73,7 @@ router.get("/github-auth-callback", async (request, response, next) => {
 			installations.filter((installation: any) => installation.target_type === "User").length === 0
 		) {
 			// if app not installed, redirect via the install permissions url
-			response.redirect(installUrl)
+			response.redirect(installUrl + "?state=" + encodeURIComponent(callbackUrl))
 		} else {
 			response.redirect(callbackUrl)
 		}
