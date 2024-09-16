fb5a1742d53745ce36ab3ad02a41fd93341288e6
Jan <jan.johannes@opral.com>
2024-09-16 19:18:13 +0300

    branch support

--- lix/packages/sdk/src/change-queue.test.ts
+++ lix/packages/sdk/src/change-queue.test.ts
@@ -75,10 +75,22 @@ test("should use queue and settled correctly", async () => {
 		providePlugins: [mockPlugin],
 	});
 
+	const branches = await lix.db.selectFrom("branch").selectAll().execute();
+
+	expect(branches).toEqual([
+		{
+			id: branches[0]?.id,
+			name: "main",
+			base_branch: null,
+			active: 1,
+		},
+	]);
+
 	const enc = new TextEncoder();
+
 	await lix.db
 		.insertInto("file")
-		.values({ id: "test", path: "test.txt", data: enc.encode("test") })
+		.values({ id: "test", path: "test.txt", data: enc.encode("test orig") })
 		.execute();
 
 	const internalFiles = await lix.db
@@ -89,6 +101,7 @@ test("should use queue and settled correctly", async () => {
 	expect(internalFiles).toEqual([]);
 
 	const queue = await lix.db.selectFrom("change_queue").selectAll().execute();
+
 	expect(queue).toEqual([
 		{
 			id: 1,
@@ -131,15 +144,34 @@ test("should use queue and settled correctly", async () => {
 			plugin_key: "mock-plugin",
 			value: {
 				id: "test",
-				text: "test",
+				text: "test orig",
 			},
 			meta: null,
-			commit_id: null,
 			operation: "create",
 		},
 	]);
 
-	// Test replacing uncommitted changes and multiple changes processing
+	const branchChanges = await lix.db
+		.selectFrom("branch_change")
+		.selectAll()
+		.execute();
+
+	expect(branchChanges).toEqual([
+		{
+			branch_id: branches[0]?.id,
+			change_id: changes[0]?.id,
+			id: branchChanges[0]?.id,
+			seq: 1,
+		},
+	]);
+
+	await lix.db
+		.updateTable("file")
+		.set({ data: enc.encode("test updated text") })
+		.where("id", "=", "test")
+		.execute();
+
+	// repeat same update
 	await lix.db
 		.updateTable("file")
 		.set({ data: enc.encode("test updated text") })
@@ -175,6 +207,13 @@ test("should use queue and settled correctly", async () => {
 			metadata: null,
 			data: queue2[1]?.data,
 		},
+		{
+			id: 5,
+			file_id: "test",
+			path: "test.txt",
+			metadata: null,
+			data: queue2[2]?.data,
+		},
 	]);
 
 	await lix.settled();
@@ -190,56 +229,123 @@ test("should use queue and settled correctly", async () => {
 
 	expect(updatedChanges).toEqual([
 		{
+			id: changes[0]?.id,
 			author: null,
-			id: updatedChanges[0]?.id,
-			created_at: updatedChanges[0]?.created_at,
+			created_at: changes[0]?.created_at,
 			parent_id: null,
 			type: "text",
 			file_id: "test",
-			operation: "create",
 			plugin_key: "mock-plugin",
 			value: {
 				id: "test",
-				text: "test",
+				text: "test orig",
 			},
 			meta: null,
-			commit_id: null,
+			operation: "create",
 		},
 		{
 			author: null,
-			commit_id: null,
-			created_at: updatedChanges[1]?.created_at,
-			file_id: "test",
 			id: updatedChanges[1]?.id,
-			meta: null,
-			operation: "update",
-			parent_id: updatedChanges[0]?.id,
-			plugin_key: "mock-plugin",
+			created_at: updatedChanges[1]?.created_at,
+			parent_id: changes[0]?.id,
 			type: "text",
+			file_id: "test",
+			operation: "update",
+			plugin_key: "mock-plugin",
 			value: {
 				id: "test",
 				text: "test updated text",
 			},
+			meta: null,
 		},
 		{
-			author: null,
-			commit_id: null,
-			created_at: updatedChanges[2]?.created_at,
-			file_id: "test",
 			id: updatedChanges[2]?.id,
-			meta: null,
-			operation: "update",
+			author: null,
 			parent_id: updatedChanges[1]?.id,
-			plugin_key: "mock-plugin",
 			type: "text",
-			value: {
-				id: "test",
-				text: "test updated text second update",
-			},
+			file_id: "test",
+			plugin_key: "mock-plugin",
+			operation: "update",
+			value: { id: "test", text: "test updated text second update" },
+			meta: null,
+			created_at: updatedChanges[2]?.created_at,
 		},
 	]);
 
-	await lix.commit({ description: "test commit" });
+	const branchChanges2 = await lix.db
+		.selectFrom("branch_change")
+		.selectAll()
+		.execute();
+
+	expect(branchChanges2).toEqual([
+		{
+			branch_id: branches[0]?.id,
+			change_id: updatedChanges[0]?.id,
+			id: branchChanges2[0]?.id,
+			seq: 1,
+		},
+		{
+			branch_id: branches[0]?.id,
+			change_id: updatedChanges[1]?.id,
+			id: branchChanges2[1]?.id,
+			seq: 2,
+		},
+		{
+			branch_id: branches[0]?.id,
+			change_id: updatedChanges[2]?.id,
+			id: branchChanges2[2]?.id,
+			seq: 3,
+		},
+	]);
+
+	// test change_view
+
+	const changesFromView = await lix.db
+		.selectFrom("change_view")
+		.where("branch_id", "=", branches[0]!.id)
+		.selectAll()
+		.execute();
+
+	expect(changesFromView).toEqual([
+		{
+			author: null,
+			id: changesFromView[0]?.id,
+			parent_id: null,
+			type: "text",
+			plugin_key: "mock-plugin",
+			operation: "create",
+			value: { id: "test", text: "test orig" },
+			meta: null,
+			created_at: changesFromView[0]?.created_at,
+			branch_id: branches[0]?.id,
+		},
+		{
+			author: null,
+			id: changesFromView[1]?.id,
+			parent_id: changesFromView[0]?.id,
+			type: "text",
+			plugin_key: "mock-plugin",
+			operation: "update",
+			value: { id: "test", text: "test updated text" },
+			meta: null,
+			created_at: changesFromView[1]?.created_at,
+			branch_id: branches[0]?.id,
+		},
+		{
+			author: null,
+			id: changesFromView[2]?.id,
+			parent_id: changesFromView[1]?.id,
+			type: "text",
+			plugin_key: "mock-plugin",
+			operation: "update",
+			value: { id: "test", text: "test updated text second update" },
+			meta: null,
+			created_at: changesFromView[2]?.created_at,
+			branch_id: branches[0]?.id,
+		},
+	]);
+
+		
 });
 
 test("changes should contain the author", async () => {
